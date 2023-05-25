e82c84f459b831e442a3e78873ce8a567d642143
Paweł <pawel.kam4@gmail.com>
2023-05-25 23:36:00 +0200

    login register changing onclick

--- Connectify/src/main/java/org/example/LoginController.java
+++ Connectify/src/main/java/org/example/LoginController.java
@@ -9,14 +9,26 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 public class LoginController {
 
+    // Pane - container of login and register layout
     @FXML
-    private Pane borderPane;
+    private Pane Pane;
+
+
+
     public void showLogin (ActionEvent event) throws IOException {
         Pane newLoadedPane =  FXMLLoader.load(getClass().getResource("logLayout.fxml"));
-        borderPane.getChildren().add(newLoadedPane);
+        Pane.getChildren().clear();
+        Pane.getChildren().add(newLoadedPane);
+    }
+
+    public void showRegister (ActionEvent event) throws IOException {
+        Pane newLoadedPane =  FXMLLoader.load(getClass().getResource("registerLayout.fxml"));
+        Pane.getChildren().clear();
+        Pane.getChildren().add(newLoadedPane);
     }
     @FXML
     private void switchToSecondary() throws IOException {
