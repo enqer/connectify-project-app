f8c2fd0f4d48d98d0e7ad16ae72fe6e82bb8bdbd
Paweł <pawel.kam4@gmail.com>
2023-05-25 23:14:54 +0200

    showing login onclick

--- Connectify/src/main/java/org/example/LoginController.java
+++ Connectify/src/main/java/org/example/LoginController.java
@@ -1,12 +1,27 @@
 package org.example;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 public class LoginController {
 
     @FXML
+    private Pane borderPane;
+    public void showLogin (ActionEvent event) throws IOException {
+        Pane newLoadedPane =  FXMLLoader.load(getClass().getResource("logLayout.fxml"));
+        borderPane.getChildren().add(newLoadedPane);
+    }
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("chat");
     }
+
+
 }
