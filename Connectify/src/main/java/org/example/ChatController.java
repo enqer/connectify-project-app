6985697ef40b55b0f54a10f43fcb1afc1bc6adb5
Narothe <110347585+Narothe@users.noreply.github.com>
2023-05-26 20:27:15 +0200

    start project

--- Connectify/src/main/java/org/example/ChatController.java
+++ Connectify/src/main/java/org/example/ChatController.java
@@ -1,12 +1,39 @@
 package org.example;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ChatController {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class ChatController implements Initializable {
+    @FXML
+    private ListView<String> myListView;
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("admin");
+    private Label myLabel;
+
+    String[] persons = {"John", "Alice", "Steve", "Paul", "Dupa_rozpruwacz_69420"};
+
+    String currentPerson;
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        myListView.getItems().addAll(persons);
+
+        myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+                currentPerson = myListView.getSelectionModel().getSelectedItem();
+
+                myLabel.setText(currentPerson);
+
+            }
+        });
+
     }
 }
