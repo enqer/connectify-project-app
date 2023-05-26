c890183623f9bdee49164c620319d29abeaa89f6
Paweł <pawel.kam4@gmail.com>
2023-05-26 13:41:36 +0200

    login css

--- Connectify/src/main/java/org/example/LoginController.java
+++ Connectify/src/main/java/org/example/LoginController.java
@@ -7,11 +7,15 @@ import java.util.Objects;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 public class LoginController {
+
+    @FXML
+    private Button loginBtn, registerBtn;
 
     // Pane - container of login and register layout
     @FXML
@@ -28,17 +32,21 @@ public class LoginController {
         Pane newLoadedPane =  FXMLLoader.load(getClass().getResource("logLayout.fxml"));
         Pane.getChildren().clear();
         Pane.getChildren().add(newLoadedPane);
+        loginBtn.setDisable(true);
+        registerBtn.setDisable(false);
     }
 
     public void showRegister (ActionEvent event) throws IOException {
         Pane newLoadedPane =  FXMLLoader.load(getClass().getResource("registerLayout.fxml"));
         Pane.getChildren().clear();
         Pane.getChildren().add(newLoadedPane);
+        loginBtn.setDisable(false);
+        registerBtn.setDisable(true);
     }
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("chat");
-    }
+//    @FXML
+//    private void switchToSecondary() throws IOException {
+//        App.setRoot("chat");
+//    }
 
 
 }
