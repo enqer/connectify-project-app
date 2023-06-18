ebaf64a3db1140cd107cdf4ee396feabc1f1f46a
Narothe <110347585+Narothe@users.noreply.github.com>
2023-06-19 01:22:53 +0200

    logout still no working

--- Connectify/src/main/java/org/example/ChatController.java
+++ Connectify/src/main/java/org/example/ChatController.java
@@ -20,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -668,7 +669,7 @@ public class ChatController implements Initializable {
         stage.show();
 
         // Zamknięcie bieżącego okna logowania
-        Stage currentStage = (Stage) myLabel.getScene().getWindow();
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         currentStage.close();
 
     }
